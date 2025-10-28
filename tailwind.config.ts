8575d3f47bcfa945eb67751f96768604edd30e26
Chinemerem Ugbaja <marycynthiaconfirm@gmail.com>
2025-10-28 16:24:16 +0000

    feat: add the nav bar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,14 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   theme: {
-    extend: {},
+    extend: {
+        color: {
+            "deep-blue": "#000c54",
+            "orange": "#f38049",
+            "nav-grey": "#babab9"
+        }
+       
+    },
   },
   plugins: [],
 };
