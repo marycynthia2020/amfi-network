38e5d819f482e3fd5ffe409836127ec42db5945e
Chinemerem Ugbaja <marycynthiaconfirm@gmail.com>
2025-10-29 01:19:23 +0000

    fix: add margin top to the content div of the hero section for a better layout

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -2,11 +2,11 @@ import Button from "../ui/Button"
 
 const Hero = () => {
   return (
-    <section className="w-full h-screen bg-[url('/hero.jpg')]  bg-no-repeat bg-cover   overflow-hidden" id="about">
+    <section className="w-full h-screen bg-[url('/hero.jpg')]  bg-no-repeat bg-cover  overflow-hidden" id="about">
        <div className="absolute inset-0 bg-linear-to-r from-black/80  to-transparent "></div>
       <div className="px-5 absolute max-w-[1440px] mx-auto  inset-0 flex items-center fade-in-up">
-         <div className="w-full max-w-4xl flex flex-col gap-10 text-white">
-          <h1 className="text-5xl md:text-6xl lg:text-7xl -mb-2  font-bold leading-tight">
+         <div className="w-full max-w-4xl flex flex-col gap-8 text-white mt-20 md:mt-0">
+          <h1 className="text-5xl md:text-6xl lg:text-7xl  font-bold leading-tight">
             Rebuilding Nigeria and <span className="text-orange">Sub-Sahara Africa</span>
           </h1>
           <p className="text-xl md:text-2xl  leading-relaxed max-w-3xl ">
